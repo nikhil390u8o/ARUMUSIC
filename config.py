55199abe8432417eeb7aeb45e5f890bce6e5f6e0
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-19 12:42:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from datetime import datetime
+
+API_ID = 33603336
+API_HASH = "c9683a8ec3b886c18219f650fc8ed429"
+BOT_TOKEN = "8411080834:AAE85QH-LpaiOpht-RSMpwYQPus1jHONnu4"
+SESSION_STRING = "BQE-4i0..."
+
+# Global Dictionaries
+queues = {}
+BOT_START_TIME = datetime.now()
